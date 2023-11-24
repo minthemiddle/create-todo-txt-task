8cc8e8f65960bb858aed40a97954e54ac118a1cc
Martin Betz <martinmichaelbetz@gmail.com>
2023-11-24 10:11:34 +0100

    Add: Reset form after submission

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { Form, ActionPanel, Action, showToast } from "@raycast/api";
 import { promises as fs } from "fs";
 import { join } from "path";
-
+import { useState } from "react";
 
 type Values = {
   textfield: string;
@@ -9,10 +9,12 @@ type Values = {
 };
 
 export default function Command() {
+  const [textField, setTextField] = useState('');
+  const [tokenEditor, setTokenEditor] = useState<string[]>([]);
+
   async function handleSubmit(values: Values) {
     const todoPath = join("/Users/martinbetz/Notes/Todos", "todo.txt");
     
-    // Check if tokeneditor has a value, otherwise, use only values.textfield
     const todoItem =
       values.tokeneditor && values.tokeneditor[0]
         ? "(" + values.tokeneditor[0] + ") " + values.textfield
@@ -23,8 +25,11 @@ export default function Command() {
       title: "Added todo",
       message: "Added todo. See logs for submitted values",
     });
-  }
 
+    // Clear the fields by updating the state
+    setTextField('');
+    setTokenEditor([]);
+  }
 
   return (
     <Form
@@ -35,8 +40,19 @@ export default function Command() {
       }
     >
       <Form.Description text="Add todo" />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="" />
-      <Form.TagPicker id="tokeneditor" title="Priority">
+      <Form.TextField
+        id="textfield"
+        title="Text field"
+        placeholder="Enter text"
+        value={textField}
+        onChange={(text) => setTextField(text)}
+      />
+      <Form.TagPicker
+        id="tokeneditor"
+        title="Priority"
+        values={tokenEditor}
+        onChange={(tokens) => setTokenEditor(tokens)}
+      >
         <Form.TagPicker.Item value="A" title="A" />
         <Form.TagPicker.Item value="B" title="B" />
         <Form.TagPicker.Item value="C" title="C" />
